7e010ff9a7ef0d3969fd0d57cff38471400bb373
mabi <crust87@gmail.com>
2015-05-22 18:20:43 +0900

    cahnge commant

--- app/src/main/java/com/mabi87/privacyactivity/PrivacyActivity.java
+++ app/src/main/java/com/mabi87/privacyactivity/PrivacyActivity.java
@@ -75,7 +75,8 @@ public abstract class PrivacyActivity extends ActionBarActivity {
 		ActivityManager am = (ActivityManager) this.getSystemService(Context.ACTIVITY_SERVICE);
 		List<RunningTaskInfo> list = am.getRunningTasks(1);
 
-		// check if this activity comes from other application package
+		// check if this activity goes other application package
+		// it's not working on lollipop. fix it!
 		for (RunningTaskInfo info : list) {
 			if (info.baseActivity.getClassName().indexOf(packageName) < 0) {
 				isBackFromHomeKey = true;
